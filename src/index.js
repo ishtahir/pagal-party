b96d35f3d48b33859862738610e5c460665d92e8
Ish Tahir <tahirismaeel@gmail.com>
2021-05-30 15:44:14 -0500

    Added Firebase and Auth Contexts over App

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './components/App';
 
 import { FirebaseContextProvider } from './contexts/FirebaseContext/FirebaseContext';
+import { AuthContextProvider } from './contexts/AuthContext/AuthContext';
 
 ReactDOM.render(
   <FirebaseContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <AuthContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AuthContextProvider>
   </FirebaseContextProvider>,
   document.getElementById('root')
 );
