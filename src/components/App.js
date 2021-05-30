640742320ff7af55af5859b421704db8bb3ce6ee
Ish Tahir <tahirismaeel@gmail.com>
2021-05-30 15:47:14 -0500

    Removed all variables not being uesd

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div className='App'></div>;
-}
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Signin from './Signin';
+import Header from './Header';
+import Gameroom from './Gameroom';
+
+const App = () => {
+  return (
+    <Router>
+      <Route exact path='/' component={Signin} />
+      <Route
+        path='/gameroom'
+        render={() => (
+          <>
+            <Header />
+            <Gameroom />
+          </>
+        )}
+      />
+    </Router>
+  );
+};
 
 export default App;
