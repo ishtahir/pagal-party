cab14cae9b649b0c674c0507434ee11dc0886a83
Ish Tahir <tahirismaeel@gmail.com>
2021-05-30 15:43:10 -0500

    Created and updated Header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import Signout from './Signout';
+
+import { AuthContext } from '../contexts/AuthContext/AuthContext';
+
+const Header = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <header className='header flex center'>
+      <h1 className='header-logo'>Playper</h1>
+      {user && <p className='header-name'>Welcome, {user.displayName}</p>}
+      <Signout />
+    </header>
+  );
+};
+
+export default Header;
