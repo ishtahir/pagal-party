a9451e740b762ff1a530f9197e29ae77ca7d1a43
Ish Tahir <tahirismaeel@gmail.com>
2021-05-31 11:28:44 -0500

    Added provider state and passing it down

--- src/contexts/AuthContext/AuthContext.js
+++ src/contexts/AuthContext/AuthContext.js
@@ -6,16 +6,18 @@ export const AuthContext = createContext(null);
 export const AuthContextProvider = (props) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [provider, setProvider] = useState(null);
 
   useEffect(() => {
     app.auth().onAuthStateChanged((userInfo) => {
       setUser(userInfo);
       setLoading(false);
+      setProvider(userInfo.providerData[0].providerId.split('.')[0]);
     });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    <AuthContext.Provider value={{ user, loading, provider }}>
       {props.children}
     </AuthContext.Provider>
   );
