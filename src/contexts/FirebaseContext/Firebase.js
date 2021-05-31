1842e952a14a2bd7dab92f3d412388656d726e7b
Ish Tahir <tahirismaeel@gmail.com>
2021-05-31 11:15:47 -0500

    Added facebook login

--- src/contexts/FirebaseContext/Firebase.js
+++ src/contexts/FirebaseContext/Firebase.js
@@ -20,16 +20,46 @@ class Firebase {
     firebase.auth().signInWithPopup(provider);
   };
 
+  facebookSignIn = async () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
+
   signOut = () => {
     firebase.auth().signOut();
   };
 
-  deleteFromCollection = async (collection, key, val) => {
+  // this will delete one document in the collection where the field is equal to the value given
+  deleteDocumentFromCollection = async (collection, field, value) => {
     await this.db
       .collection(collection)
-      .where(key, '==', val)
+      .where(field, '==', value)
       .get()
       .then((snap) => snap.forEach((shot) => shot.ref.delete()));
+  };
+
+  // this will delete the field given from every document in the collection given
+  deleteFieldFromDocument = async (collection, field) => {
+    await this.db
+      .collection(collection)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          doc.ref.update({
+            [field]: this.app.firebase_.firestore.FieldValue.delete(),
+          });
+        });
+      });
+  };
+
+  // this will update a single field with the given value
+  updateSettings = async (field, value) => {
+    await this.db
+      .collection('settings')
+      .doc('3Fd2IrMcifnJjBYJfcJc')
+      .update({
+        [field]: value,
+      });
   };
 }
 
