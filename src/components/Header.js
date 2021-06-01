922c161e4ff3f64d32588f10a9c516c7a8813e1a
Ish Tahir <tahirismaeel@gmail.com>
2021-05-31 22:52:51 -0500

    Using the chosenName from user

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,13 @@ import Signout from './Signout';
 
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
 
-const Header = () => {
+const Header = ({ name }) => {
   const { user } = useContext(AuthContext);
 
   return (
     <header className='header flex center'>
       <h1 className='header-logo'>Playper</h1>
-      {user && (
-        <p className='header-name'>
-          Welcome, {user.chosenName ? user.chosenName : user.displayName}
-        </p>
-      )}
+      {user && <p className='header-name'>Welcome, {name ? name : 'user'}</p>}
       <Signout />
     </header>
   );
