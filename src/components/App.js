325f88316d6348e5fd18016be4eb4d910484b378
Ish Tahir <tahirismaeel@gmail.com>
2021-06-05 14:33:59 -0500

    Root site now takes you to Rooms component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Signin from './Signin';
+import Rooms from './Rooms';
 import Gameroom from './Gameroom';
 
 const App = () => {
   return (
     <Router>
-      <Route exact path='/' component={Signin} />
+      <Route exact path='/' component={Rooms} />
       <Route path='/gameroom' component={Gameroom} />
     </Router>
   );
