0ab2322dcafcb4b55b0a4bb1d3f23b1f3ac1231f
Ish Tahir <tahirismaeel@gmail.com>
2021-06-05 12:48:45 -0500

    Bug fixes

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -15,11 +15,13 @@ function shuffle(array) {
   return array;
 }
 
-export function createHitler(players) {
-  if (players < 5 || players > 10)
+export const createHitler = (players) => {
+  // if (players < 5 || players > 10)
+  if (players < 3 || players > 3)
     return 'Please enter correct amount of players.';
 
   const chars = {
+    3: [2, 0], // remove this
     5: [3, 1],
     6: [4, 1],
     7: [4, 2],
@@ -38,11 +40,13 @@ export function createHitler(players) {
   const result = [];
 
   for (let i = 0; i < chars[players][0]; i++) {
-    result.push(envelope('liberal', 'liberal'));
+    const role = 'liberal';
+    result.push(envelope(role, role));
   }
 
   for (let i = 0; i < chars[players][1]; i++) {
-    result.push(envelope('fascist', 'fascist'));
+    const role = 'fascist';
+    result.push(envelope(role, role));
   }
 
   result.push(envelope('fascist', 'hitler'));
@@ -52,4 +56,19 @@ export function createHitler(players) {
   }
 
   return result;
-}
+};
+
+export const getDocId = (arr, key, val) => {
+  return arr.filter((item) => item[key] === val)[0].id;
+};
+
+export const createRoomName = () => {
+  let room = '';
+  const chars = 'abcdefghijklmnopqrstuvwxyz';
+
+  for (let i = 0; i < 4; i++) {
+    const ind = Math.floor(Math.random() * chars.length);
+    room += chars[ind];
+  }
+  return room.toUpperCase();
+};
