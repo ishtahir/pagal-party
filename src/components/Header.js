f441944e16b4c21de19c68ee1805231d0c756a50
Ish Tahir <tahirismaeel@gmail.com>
2021-06-08 00:08:28 -0500

    Showing Signout component conditionally

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import { useContext } from 'react';
-import Signout from './Signout';
 
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
 
-const Header = ({ name }) => {
+import Signout from './Signout';
+
+const Header = ({ roomid }) => {
   const { user } = useContext(AuthContext);
 
   return (
     <header className='header flex center'>
       <h1 className='header-logo'>Playper</h1>
-      {user && <p className='header-name'>Welcome, {name ? name : 'user'}</p>}
-      <Signout />
+      {user && <Signout roomid={roomid} />}
     </header>
   );
 };
