7c091138a1f22ab86aac9ca4ba536185341e60f4
Ish Tahir <tahirismaeel@gmail.com>
2021-06-08 14:57:38 -0500

    Removed all create/join rooms logic

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,74 +1,57 @@
 import { useContext } from 'react';
 
-import { createRoomName } from '../utils/functions';
-
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { FirebaseContext } from '../contexts/FirebaseContext/FirebaseContext';
-import { AuthContext } from '../contexts/AuthContext/AuthContext';
 
-import Signout from './Signout';
-import { Redirect } from 'react-router';
+import { Redirect, useParams } from 'react-router-dom';
 
-const Games = () => {
+const Games = (props) => {
   const { db } = useContext(FirebaseContext);
-  const { user } = useContext(AuthContext);
+  const { roomid } = useParams();
 
   const [games] = useCollectionData(
     db.collection('games').orderBy('createdAt'),
     { idField: 'id' }
   );
+
   const [rooms] = useCollectionData(
     db.collection('rooms').orderBy('createdAt'),
     { idField: 'id' }
   );
+  const gameSelected =
+    rooms && rooms.length && rooms.filter((room) => room.id === roomid)[0].game;
 
-  const playerAssignedRoom =
-    rooms &&
-    rooms.length &&
-    user &&
-    rooms.filter((room) => room.vip === user.uid);
-
-  const playerAssignedRoomExists =
-    playerAssignedRoom && playerAssignedRoom.length > 0;
-
-  const createRoom = (e) => {
-    let newRoomName;
-    let roomExists;
-
-    do {
-      newRoomName = createRoomName();
-      roomExists =
-        rooms && rooms.filter((room) => room.id === newRoomName).length > 0; //eslint-disable-line
-    } while (roomExists);
-
-    db.collection('rooms').doc(newRoomName).set({
-      game: e.target.textContent,
-      vip: user.uid,
-      createdAt: new Date().toISOString(),
-    });
-
-    // db.collection('players').doc(user.uid).update({ room: newRoomName });
+  const addGameToRoom = async (e) => {
+    const game = e.target.textContent;
+    await db.collection('rooms').doc(roomid).update({ game });
   };
 
   return (
     <div className='flex col center'>
-      {console.log({ playerAssignedRoom, playerAssignedRoomExists })}
-      {console.log(rooms && rooms[0])}
-      {!playerAssignedRoomExists ? (
+      {!gameSelected ? (
         <>
           <h1>Select game to play</h1>
           {games &&
             games.map((game) => (
-              <button key={game.id} className='btn m2-t' onClick={createRoom}>
+              <button
+                key={game.id}
+                className='btn m2-t'
+                onClick={addGameToRoom}
+              >
                 {game.name}
               </button>
             ))}
         </>
       ) : (
-        <Redirect push to={`/rooms/${playerAssignedRoom[0].id}`} />
+        <Redirect
+          push
+          to={`/rooms/${roomid}/games/${gameSelected
+            .toLowerCase()
+            .split(' ')
+            .join('-')}`}
+        />
       )}
-      <Signout />
     </div>
   );
 };
