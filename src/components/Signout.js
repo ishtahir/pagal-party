adbcfb8bf531dd41ae0bc6cb0417e8ec111445a7
Ish Tahir <tahirismaeel@gmail.com>
2021-06-08 14:56:20 -0500

    If you're owner of a room, whole room gets deleted along with players in that room

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -1,24 +1,41 @@
 import { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 
-import { AuthContext } from '../contexts/AuthContext/AuthContext';
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 import { FirebaseContext } from '../contexts/FirebaseContext/FirebaseContext';
+import { AuthContext } from '../contexts/AuthContext/AuthContext';
 
 const Signout = ({ roomid }) => {
-  const { user } = useContext(AuthContext);
-  const { signOutFromApp, deleteDocumentFromCollection } =
+  const { db, signOutFromApp, deleteDocumentFromCollection } =
     useContext(FirebaseContext);
+  const { user } = useContext(AuthContext);
+
+  const [rooms] = useCollectionData(db.collection('rooms'), { idField: 'id' });
+  const roomExists =
+    rooms && rooms.length && rooms.filter((room) => room.id === roomid);
+  const owner =
+    user && roomExists && roomExists.length && roomExists[0].owner === user.uid;
 
   const handleClick = async () => {
     const { uid } = user;
 
     if (roomid) {
-      await deleteDocumentFromCollection('rooms', 'vip', uid);
-      await deleteDocumentFromCollection('players', 'room', roomid);
+      if (owner) {
+        await deleteDocumentFromCollection('rooms', 'owner', uid);
+        await deleteDocumentFromCollection('players', 'room', roomid);
+      } else {
+        await deleteDocumentFromCollection('players', 'uid', uid);
+        await db
+          .collection('rooms')
+          .doc(roomid)
+          .update({
+            players: firebase.firestore.FieldValue.arrayRemove(uid),
+          });
+      }
       await signOutFromApp();
     }
-
-    localStorage.clear();
   };
 
   return user ? (
@@ -26,7 +43,7 @@ const Signout = ({ roomid }) => {
       Sign out
     </button>
   ) : (
-    <Redirect to='/' />
+    <Redirect push to='/' />
   );
 };
 
