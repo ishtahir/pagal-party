2421c2d0fca9ea65e73eef70f8fd1191f0b6e286
Ish Tahir <tahirismaeel@gmail.com>
2021-06-07 23:45:23 -0500

    Removed all game logic and  simplified signing out

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -1,46 +1,24 @@
 import { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
 import { FirebaseContext } from '../contexts/FirebaseContext/FirebaseContext';
 
-const Signout = () => {
+const Signout = ({ roomid }) => {
   const { user } = useContext(AuthContext);
-  const {
-    db,
-    signOutFromApp,
-    deleteDocumentFromCollection,
-    // deleteFieldFromDocument,
-    // gameOver,
-  } = useContext(FirebaseContext);
+  const { signOutFromApp, deleteDocumentFromCollection } =
+    useContext(FirebaseContext);
 
-  const settingsRef = db.collection('settings');
-  const [gameSettings] = useCollectionData(settingsRef, { idField: 'id' });
-
-  const gameStarted = gameSettings && gameSettings[0].gameStarted;
-
-  const handleClick = () => {
+  const handleClick = async () => {
     const { uid } = user;
 
-    if (gameStarted) {
-      // const approved = window.confirm(
-      //   '⛔️WARNING⛔️: By signing out, the current game will end for all players! Do you still want to proceed?'
-      // );
-      // console.log({ approved });
-      // if (approved) {
-      //   console.log('inside approved block');
-      //   gameOver();
-      //   deleteFieldFromDocument('players', 'cards');
-      //   deleteDocumentFromCollection('players', 'uid', uid);
-      //   signOutFromApp();
-      // }
-      alert('PLEASE NOTIFY THE VIP TO END THE GAME SO YOU CAN SIGN OUT!');
-    } else {
-      deleteDocumentFromCollection('players', 'uid', uid);
-      signOutFromApp();
+    if (roomid) {
+      await deleteDocumentFromCollection('rooms', 'vip', uid);
+      await deleteDocumentFromCollection('players', 'room', roomid);
+      await signOutFromApp();
     }
+
+    localStorage.clear();
   };
 
   return user ? (
