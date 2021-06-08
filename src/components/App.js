7790984e80c5d6dc62d26960f86fb4a3cd906347
Ish Tahir <tahirismaeel@gmail.com>
2021-06-07 23:48:00 -0500

    Add games route and component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Gameroom from './Gameroom';
+import Games from './Games';
 
 const App = () => {
   return (
     <Router>
       <Route exact path='/' component={Home} />
+      <Route path='/games' component={Games} />
       <Route path='/rooms/:roomid' render={() => <Gameroom />} />
     </Router>
   );
