9ec44f2282ca9e92cefb4d593ef065b600fddfef
Ish Tahir <tahirismaeel@gmail.com>
2021-06-08 14:50:22 -0500

    Added routes for rooms, updated routes for games/gameroom

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Gameroom from './Gameroom';
 import Games from './Games';
+import Rooms from './Rooms';
 
 const App = () => {
   return (
     <Router>
       <Route exact path='/' component={Home} />
-      <Route path='/games' component={Games} />
-      <Route path='/rooms/:roomid' render={() => <Gameroom />} />
+      <Route exact path='/rooms' component={Rooms} />
+      <Route exact path='/rooms/:roomid/games' component={Games} />
+      <Route
+        exact
+        path='/rooms/:roomid/games/:game'
+        render={() => <Gameroom />}
+      />
     </Router>
   );
 };
