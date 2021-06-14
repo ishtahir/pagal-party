45c27c7e7634f4bfc7bc492e26dc419f471c26dd
Ish Tahir <tahirismaeel@gmail.com>
2021-06-14 13:24:21 -0500

    Created Card component for abstraction

---
+++ src/components/games/secret-hitler/Card.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+
+const Card = ({ player, type }) => {
+  const [side, setSide] = useState('back');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (side !== 'back') {
+        setSide('back');
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [side]); // eslint-disable-line
+
+  const role = (card) => {
+    if (player && player.hasOwnProperty('cards')) {
+      return card === 'party' ? player.cards.party : player.cards.secret;
+    }
+    return '';
+  };
+
+  const flip = (e) => {
+    const card = e.target.id;
+
+    if (card === 'party-card') {
+      if (side === 'back') {
+        setSide(role(card));
+      }
+    } else if (card === 'secret-card') {
+      if (side === 'back') {
+        const approve = window.confirm(
+          'ARE YOU SURE YOU WANT TO FLIP OVER YOUR SECRET ROLE?'
+        );
+        if (approve) {
+          setSide(role(card));
+        }
+      }
+    }
+  };
+
+  // const cardStyles =
+  //   'card border border-solid border-gray-300 rounded-2xl flex justify-center items-center cursor-pointer text-center absolute relative h-full w-full';
+  // const frontStyles = 'front items-end cursor-not-allowed';
+  // const backStyles = 'back';
+
+  return (
+    <div className='card-flip-wrap w-60 h-72'>
+      <div className={side === 'back' ? 'card-flip' : 'card-flip is-flipped'}>
+        <div
+          className={`card-face card ${type} back`}
+          id={`${type}-card`}
+          onClick={flip}
+        />
+        <div className={`card-face card ${type} front ${role(type)}`} />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
