62e3028c7af5d5735f1e8b17e7f9b2b288be73bc
Ish Tahir <tahirismaeel@gmail.com>
2021-06-14 01:44:59 -0500

    Replaced all text with new components

--- src/components/Gameroom.js
+++ src/components/Gameroom.js
@@ -9,7 +9,8 @@ import { AuthContext } from '../contexts/AuthContext/AuthContext';
 
 import Join from './Join';
 import SecretHitler from './games/secret-hitler/SecretHitler';
-// import GameOfThings from './games/game-of-things/GameOfThings';
+
+import Text from './elements/Text';
 
 const Gameroom = () => {
   const { roomid } = useParams();
@@ -27,29 +28,36 @@ const Gameroom = () => {
   const showGame = (game) => {
     if (game === 'Secret Hitler') {
       return <SecretHitler roomData={room} />;
-    } // else if (game === 'The Game of Things') {
-    // return <GameOfThings roomData={room} />;
-    // }
+    }
   };
 
   return loading ? (
     <div className='loading'></div>
   ) : user ? (
-    <div className='flex col center'>
+    <>
       {loadRoom ? (
         <div className='loading'></div>
       ) : room ? (
-        <div className='m5-y flex col center'>
-          <h1>Room</h1>
-          <div className='gr-roomname'>{roomid}</div>
-          <h2 className='m5-y gr-game'>{room && room.game}</h2>
+        <div className='flex flex-col justify-center items-center'>
+          <Text type='h2' text='ROOM' />
+          <Text
+            className='gr-roomname !text-7xl my-5'
+            type='h1'
+            text={roomid}
+          />
+          <Text type='h3' text='is playing' />
+          <Text
+            className='gr-game my-5 !text-4xl text-red-600 border-4 border-red-600 border-solid p-4 rounded-md'
+            type='h2'
+            text={room && room.game}
+          />
           {!gameStarted ? <Join roomid={roomid} /> : null}
           {showGame(game)}
         </div>
       ) : (
         <Redirect to='/' />
       )}
-    </div>
+    </>
   ) : (
     <Redirect to='/' />
   );
