71beb49f778e703a19c44836c00dbd3672dc96a9
Ish Tahir <tahirismaeel@gmail.com>
2021-06-13 20:28:19 -0500

    Created Button component with tw classes

---
+++ src/components/elements/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ className, text, handler }) => {
+  const styles =
+    'bg-pink-500 px-10 py-2 rounded uppercase text-sm text-white font-bold inline-block btn hover:bg-white hover:text-pink-500';
+  return (
+    <button
+      className={className ? `${className} ${styles}` : styles}
+      onClick={handler}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
