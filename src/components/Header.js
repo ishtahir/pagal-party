5cb0cb3a3bcc58ae837dad8c27d8e0c789e79891
Ish Tahir <tahirismaeel@gmail.com>
2021-06-13 19:04:37 -0500

    Removed my custom classes for tailwind classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <header className='header flex center'>
-      <h1 className='header-logo'>Playper</h1>
+    <header className='header flex justify-between py-5 mb-5'>
+      <h1 className='header-logo ml-5 text-3xl cursor-pointer'>Playper</h1>
       {user && <Signout />}
     </header>
   );
