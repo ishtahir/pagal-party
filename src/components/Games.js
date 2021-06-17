f8e64ee2d204e77a33146ef8936331d70ba9ad5a
Ish Tahir <tahirismaeel@gmail.com>
2021-06-17 16:12:36 -0500

    Added createRoom logic, along with game name and game settings

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,12 +1,14 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { FirebaseContext } from '../contexts/FirebaseContext/FirebaseContext';
 
-import { Redirect, useParams } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import {
+  createRoomName,
+  getDate,
   secretHitlerSettings,
   scattergoriesSettings,
 } from '../utils/functions';
@@ -16,7 +18,6 @@ import Text from './elements/Text';
 
 const Games = () => {
   const { db } = useContext(FirebaseContext);
-  const { roomid } = useParams();
 
   const [games] = useCollectionData(
     db.collection('games').orderBy('createdAt'),
@@ -27,21 +28,50 @@ const Games = () => {
     db.collection('rooms').orderBy('createdAt'),
     { idField: 'id' }
   );
-  const gameSelected =
-    rooms && rooms.length && rooms.filter((room) => room.id === roomid)[0].game;
 
-  const addGameToRoom = async (e) => {
+  const [roomCreated, setRoomCreated] = useState('');
+  const [gameSelected, setGameSelected] = useState('');
+  const [connectToRoom, setConnectToRoom] = useState(false);
+
+  const createRoom = async (e) => {
     const game = e.target.textContent;
-    if (game === 'Secret Hitler') {
-      await db.collection('rooms').doc(roomid).update(secretHitlerSettings());
-    } else if (game === 'Scattergories') {
-      await db.collection('rooms').doc(roomid).update(scattergoriesSettings());
+    setGameSelected(game);
+
+    let newRoomName;
+    let roomExists;
+
+    do {
+      newRoomName = createRoomName();
+      roomExists =
+        rooms && rooms.filter((room) => room.id === newRoomName).length > 0; //eslint-disable-line
+    } while (roomExists);
+
+    setRoomCreated(newRoomName);
+
+    await db.collection('rooms').doc(newRoomName).set({
+      game,
+      players: [],
+      createdAt: getDate(),
+    });
+
+    if (gameSelected === 'Secret Hitler') {
+      await db
+        .collection('rooms')
+        .doc(newRoomName)
+        .update(secretHitlerSettings());
+    } else if (gameSelected === 'Scattergories') {
+      await db
+        .collection('rooms')
+        .doc(newRoomName)
+        .update(scattergoriesSettings());
     }
+
+    setConnectToRoom(true);
   };
 
   return (
     <div className='games flex flex-col justify-center items-center h-2/4'>
-      {!gameSelected ? (
+      {!connectToRoom ? (
         <>
           <Text className='my-5' type='h2' text='Select game to play' />
           {games &&
@@ -50,14 +80,14 @@ const Games = () => {
                 key={game.id}
                 className='my-5'
                 text={game.name}
-                handler={addGameToRoom}
+                handler={createRoom}
               />
             ))}
         </>
       ) : (
         <Redirect
           push
-          to={`/rooms/${roomid}/games/${gameSelected
+          to={`/rooms/${roomCreated}/${gameSelected
             .toLowerCase()
             .split(' ')
             .join('-')}`}
