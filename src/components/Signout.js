ddb9acd2256fe379f4042adda8f9015dfd67812f
Ish Tahir <tahirismaeel@gmail.com>
2021-06-17 20:21:43 -0500

    Added name in sign out button

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -2,7 +2,10 @@ import { useContext } from 'react';
 import { Redirect, useLocation } from 'react-router-dom';
 
 import firebase from 'firebase/app';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
+import {
+  useDocumentData,
+  useCollectionData,
+} from 'react-firebase-hooks/firestore';
 
 import { FirebaseContext } from '../contexts/FirebaseContext/FirebaseContext';
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
@@ -13,8 +16,20 @@ import Button from './elements/Button';
 const Signout = () => {
   const { db, signOutFromApp, deleteDocumentFromCollection } =
     useContext(FirebaseContext);
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const modal = useModal();
+
+  const [players, loadPlayers] = useCollectionData(db.collection('players'), {
+    idField: 'id',
+  });
+
+  const currentPlayer =
+    !loading &&
+    !loadPlayers &&
+    players.filter((player) => player.id === user.uid);
+
+  const name =
+    !loading && currentPlayer.length ? currentPlayer[0].name : user.displayName;
 
   const roomid = useLocation().pathname.split('/')[2];
 
@@ -52,7 +67,7 @@ const Signout = () => {
   return user ? (
     <Button
       className='mr-0 md:mr-5 !bg-blue-600 hover:!bg-white hover:!text-blue-600'
-      text='Sign out'
+      text={`Sign out, ${name}`}
       handler={handleClick}
     />
   ) : (
