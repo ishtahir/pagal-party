130df7956f0f4b9c1f40b5e51cd6d719891b1ef2
Ish Tahir <tahirismaeel@gmail.com>
2021-06-18 13:58:24 -0500

    Glassified Gameroom text

--- src/components/Gameroom.js
+++ src/components/Gameroom.js
@@ -41,19 +41,21 @@ const Gameroom = () => {
       {loadRoom ? (
         <div className='loading'></div>
       ) : room ? (
-        <div className='gameroom flex flex-col justify-center items-center'>
-          <Text type='h2' text='ROOM' />
-          <Text
-            className='gr-roomname !text-7xl my-5'
-            type='h1'
-            text={roomid}
-          />
-          <Text type='h3' text='is playing' />
-          <Text
-            className='gr-game my-5 !text-4xl text-red-600 border-4 border-red-600 border-solid p-4 rounded-md bg-white'
-            type='h2'
-            text={room && room.game}
-          />
+        <div className='gameroom flex flex-col justify-center items-center my-10'>
+          <div className='glass rounded-xl p-10 text-gray-50'>
+            <Text type='h2' text='ROOM' />
+            <Text
+              className='gr-roomname !text-7xl my-5'
+              type='h1'
+              text={roomid}
+            />
+            <Text type='h3' text='is playing' />
+            <Text
+              className='gr-game my-5 !text-4xl text-purple-800 border-4 border-purple-800 border-solid p-4 rounded-md text-shadow'
+              type='h2'
+              text={room && room.game}
+            />
+          </div>
           {!gameStarted ? <Join roomid={roomid} /> : null}
           {showGame(game)}
         </div>
