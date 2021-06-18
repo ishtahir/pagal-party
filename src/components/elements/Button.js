f2c209c78d981980ac63ca4e51ac7db49169d7db
Ish Tahir <tahirismaeel@gmail.com>
2021-06-18 14:25:31 -0500

    Updated default styles

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ className, text, handler, rest }) => {
   const styles =
-    'bg-pink-500 px-10 py-2 rounded uppercase text-sm text-white font-bold inline-block btn hover:bg-white hover:text-pink-500';
+    'bg-white px-10 py-2 rounded uppercase text-sm text-pink-500 font-bold inline-block btn hover:bg-pink-100';
   return (
     <button
       className={className ? `${className} ${styles}` : styles}
