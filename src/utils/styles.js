d3bc01334977ccd2bfd9a00f26efdbfa849a45d4
Ish Tahir <tahirismaeel@gmail.com>
2021-06-19 16:44:43 -0500

    Added disabled tw styles

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -4,6 +4,9 @@
 export const glassStyles =
   'glass rounded-xl w-10/12 md:w-8/12 mx-auto text-gray-50 px-10 py-10';
 
+export const disabledStyles =
+  'disabled:bg-gray-400 disabled:text-white disabled:cursor-not-allowed disabled:transform:none disabled:translate-y-0 disabled';
+
 /****************************************************************************
  HOME COMPONENT
 *****************************************************************************/
